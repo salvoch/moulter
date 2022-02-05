d5b5709374705f8c01731e8c251e4a3bd34fe0ac
salvoch <github@salvoch.com>
2022-02-05 15:56:14 -0700

    add http host test

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import chevron
 from pathlib import Path
+import http.server
+import socketserver
+from functools import partial
 
 def render_template(filename, data):
     '''Render a template with given data. return rendered template. No error checking'''
@@ -11,14 +14,22 @@ def render_template(filename, data):
 
 def save_render(filename, render):
     '''save render to a file'''
-    template_path = Path("site/")
-    template_path.mkdir(exist_ok=True) #Create directory if it doesn't exist
-    with open(template_path / filename, 'w') as f:
+    render_path = Path("site/")
+    render_path.mkdir(exist_ok=True) #Create directory if it doesn't exist
+    with open(render_path / filename, 'w') as f:
         f.write(render)
+
+def host_site_test(sitename, path="site", PORT = 8080):
+    '''host site port 8080'''
+    handler = partial(http.server.SimpleHTTPRequestHandler, directory=path)
+    with socketserver.TCPServer(("", PORT), handler) as httpd:
+        print(f"serving at: http://localhost:{str(PORT)}/{sitename}")
+        httpd.serve_forever()
 
 if __name__ == '__main__':
     #quick test
-    testdata = {'title': 'shi-title', 'header-one': 'shi-header', 'body': 'shi-bod'}
+    testdata = {'title': 'shi-title', 'header-one': 'shi-header', 'body': 'shi-boddy'}
     render = render_template('test1.html', testdata)
     save_render('shi.html', render)
+    host_site_test('shi.html')
 
